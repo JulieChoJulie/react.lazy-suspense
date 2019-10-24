631eb6944ff9b5a0ee362288728839c1de63a55c
JulieChoJulie <h32cho@gmail.com>
2019-10-24 15:55:19 -0400

    code spliting without react.lazy

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import notify from './notify';
 
-function App() {
-  const onClick = () => {
-    // nofity();
-
-    // import를 상단에 하지 않고 import()함수 형태로 메서드 안에서 사용하면,
-    // 파일을 따로 분리시켜 저장합니다. 그리고 실제 함수가 필요한 지점에
-    // 파일을 불러와 함수를 사용할 수 있습니다.
-    // import를 함수로 사용하면 Promise를 반환합니다.
-    // stage-3단계에 있는 dynamic import라는문법
-    
-    import('./notify').then(result => result.default());
+class App extends Component {
+  state = {
+    SplitMe: null
   };
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>Hello React!</p>
-      </header>
-    </div>
-  );
+
+  handleClick = async () => {
+    const loadedModule = await import('./SplitMe');
+    this.setState({
+      SplitMe: loadedModule.default
+    });
+  };
+
+  render() {
+    const { SplitMe } = this.state;
+    return (
+        <div className="App">
+          <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+            <p onClick={this.handleClick}>Hello</p>
+            {SplitMe && <SplitMe />}
+          </header>
+        </div>
+    )
+  }
 }
 
 export default App;
