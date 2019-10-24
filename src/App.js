c976ee10e1c11c0f8bb1bbb455ffeb3f6839c54a
JulieChoJulie <h32cho@gmail.com>
2019-10-24 18:23:22 -0400

    preload

--- src/App.js
+++ src/App.js
@@ -13,13 +13,17 @@ function App() {
   const [visible, setVisible] = useState(false);
   const onClick = () => {
     setVisible(true);
+  };
+
+  const onMouseOver = () => {
+    SplitMe.preload();
   }
 
   return (
       <div className="App">
         <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={onClick}>Hello</p>
+          <p onClick={onClick} onMouseOver={onMouseOver}>Hello</p>
           {/*<Suspense fallback={<div>loading... </div>}>*/}
             {/*{visible && <SplitMe />}*/}
           {/*</Suspense>*/}
